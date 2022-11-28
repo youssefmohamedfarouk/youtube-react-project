978e81ba50bb7e29a3dca7af4b95a0e8e010e9ff
cmm-7 <carlosmendoza@pursuit.org>
2022-11-28 10:13:31 -0800

    update and bug fixes to modal and search bar

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { getSearch } from "../../api/fetch";
 import { useNavigate } from "react-router-dom";
+import Modal from "../Modal/Modal";
 
 export default function SearchBar({ setVidsArray }) {
   const [textInput, setTextInput] = useState("");
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  let errorMessage;
 
   function handleTextChange(event) {
     setTextInput(event.target.value);
@@ -17,6 +20,8 @@ export default function SearchBar({ setVidsArray }) {
       .then((data) => setVidsArray(data.items))
       //   .then(console.log)
       .catch((error) => {
+        setShowModal(!showModal)
+        errorMessage = error;
         console.error(error);
       });
     navigate(`/searches/${textInput}`);
@@ -27,7 +32,16 @@ export default function SearchBar({ setVidsArray }) {
       <form onSubmit={handleSubmit}>
         <label htmlFor="search-input">Search: </label>
         <input type="text" id="search-input" onChange={handleTextChange} />
+        <button> Search </button>
       </form>
+      <div>
+        <button onClick={() => setShowModal(!showModal)}>Modal</button>
+      </div>
+      {showModal && (
+        <Modal showModal={showModal} setShowModal={setShowModal}>
+          {errorMessage}
+        </Modal>
+      )}
     </div>
   );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,13 +2,11 @@ import { useEffect, useState } from "react";
 // import YouTube from "react-youtube";
 import { getMostPopular } from "../../api/fetch";
 
-import Modal from "../Modal/Modal";
-
 import VideoListing from "../videos/VideoListing";
 import "../videos/VideoListing.css";
 
 export default function Home({vidsArray, setVidsArray}) {
-  const [showModal, setShowModal] = useState(false);
+ 
 
   useEffect(() => {
     setVidsArray([]);
@@ -18,12 +16,6 @@ export default function Home({vidsArray, setVidsArray}) {
 
   return (
     <>
-      <div>
-        <button onClick={() => setShowModal(!showModal)}>Modal</button>
-        <Modal showModal={showModal} setShowModal={setShowModal}>
-          Here is the Modal
-        </Modal>
-      </div>
       <div className="video-container">
         {vidsArray.map((vid) => {
           return (
